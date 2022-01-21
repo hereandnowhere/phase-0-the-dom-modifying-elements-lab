dcbfa59c9314233785d963ea9f8d04513d199624
Jacob Vine <jvine1235@gmail.com>
2022-01-21 16:14:26 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector(`main`);
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute(`id`,`victory`);
+newHeader.innerHTML = 
+    "<h1 id = victory> JACOB is the champion </h1>";
+document.head.append(newHeader);
